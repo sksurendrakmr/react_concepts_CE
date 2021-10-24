a293cfe98efb74c5a0695ff3cef4c83f86f49e94
sksurendrakmr <sksurendrakmr@gmail.com>
2021-10-24 23:25:52 +0530

    consuming multiple context values

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import ComponentC from "./reactContext/ComponentC";
 import { UserProvider } from "./reactContext/UserContext";
 
+export const ThemeContext = React.createContext();
 function App() {
   return (
     <div className='App'>
       <UserProvider value='Surendra'>
-        <ComponentC />
+        <ThemeContext.Provider value={"default"}>
+          <ComponentC />
+        </ThemeContext.Provider>
       </UserProvider>
     </div>
   );
--- src/reactContext/ComponentF.js
+++ src/reactContext/ComponentF.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ThemeContext } from "../App";
 import { UserConsumer } from "./UserContext";
 
 const ComponentF = () => {
@@ -6,7 +7,17 @@ const ComponentF = () => {
     <>
       <UserConsumer>
         {(userName) => {
-          return <div>Hello ${userName}</div>;
+          return (
+            <ThemeContext.Consumer>
+              {(theme) => {
+                return (
+                  <h2>
+                    {userName} - {theme}
+                  </h2>
+                );
+              }}
+            </ThemeContext.Consumer>
+          );
         }}
       </UserConsumer>
     </>
