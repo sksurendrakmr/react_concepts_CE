4ea0f7f665d36af3495186d1b9bb3c8d27f18b5c
sksurendrakmr <sksurendrakmr@gmail.com>
2021-10-24 23:01:20 +0530

    Passing default value to context

--- src/reactContext/UserContext.js
+++ src/reactContext/UserContext.js
@@ -2,11 +2,19 @@
  * 1. Create the context
  * 2. Provide a context value
  * 3. Consume the context value
+ *
+ * we can set default value to our context and default
+ * value can be set while creating the context.
+ * It is passsed as an argument to createContext().
+ *
+ * This default value will only be used when a component
+ * does not have a matching provider above it in the
+ * component tree.
  */
 
 import React from "react";
 
-const UserContext = React.createContext();
+const UserContext = React.createContext("Suri");
 const UserProvider = UserContext.Provider;
 const UserConsumer = UserContext.Consumer;
 
