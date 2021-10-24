363738516ef31f6a7adf0cd2f5d4f942234d6fec
sksurendrakmr <sksurendrakmr@gmail.com>
2021-10-24 23:10:59 +0530

    Consuming context value using contextType

--- src/reactContext/ComponentE.js
+++ src/reactContext/ComponentE.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import ComponentF from "./ComponentF";
+import UserContext from "./UserContext";
 
-const ComponentE = () => {
-  return (
-    <div>
-      <h2>Component E</h2>
-      <ComponentF />
-    </div>
-  );
-};
+class ComponentE extends Component {
+  //if the application supports the public class field syntax then we can replace component.contextType with static
+  // static contextType = UserContext
+  render() {
+    return (
+      <div>
+        Component E context {this.context}
+        <ComponentF />
+      </div>
+    );
+  }
+}
+ComponentE.contextType = UserContext; //By doing so, in the render method the userContext value is available as this.context
 
 export default ComponentE;
--- src/reactContext/UserContext.js
+++ src/reactContext/UserContext.js
@@ -10,6 +10,12 @@
  * This default value will only be used when a component
  * does not have a matching provider above it in the
  * component tree.
+ *
+ * Another way to consume Context value is using the
+ * context type property on a class.
+ *
+ * But this approach only work with class components and with this
+ * approach we can only subscribe to a single context (using contextType)
  */
 
 import React from "react";
@@ -19,3 +25,4 @@ const UserProvider = UserContext.Provider;
 const UserConsumer = UserContext.Consumer;
 
 export { UserProvider, UserConsumer };
+export default UserContext;
